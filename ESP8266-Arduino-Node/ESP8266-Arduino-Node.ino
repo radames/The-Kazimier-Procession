4e8750c79daf65acb0c9c3a9920cb3e6c3c97a80
Radamés Ajna <radamajna@gmail.com>
2016-08-22 18:18:01 +0100

    Adding python server and Arduino nodes code

---
+++ ESP8266-Arduino-Node/ESP8266-Arduino-Node.ino
@@ -0,0 +1,198 @@
+#include <Adafruit_NeoPixel.h>
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#include <OSCMessage.h>
+#include <OSCBundle.h>
+#include <OSCData.h>
+#include "WifiPass.h"
+
+#ifdef __AVR__
+#include <avr/power.h>
+#endif
+
+#define BUTTON 5 //GPIO5
+#define PIN 4  //GPIO4
+#define PWMPIN 0  //GPIO0
+#define NUMPIXELS 27
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+//
+
+// A UDP instance to let us send and receive packets over UDP
+
+WiFiUDP Udp;
+const IPAddress outIp(192, 168, 20, 3);  // remote IP (not needed for receive) kronosServer static IP
+
+
+const unsigned int inPort = 8888;
+const unsigned int outPort = 9999;
+
+OSCErrorCode error;
+unsigned int ledState = HIGH;              // LOW means led is *on*
+
+//node s
+enum NodeState { CONNECT, WAIT, DISCONNECT };
+
+NodeState nState = CONNECT;
+long lastMillis = 0;
+
+void setup() {
+  pinMode(BUILTIN_LED, OUTPUT);
+  digitalWrite(BUILTIN_LED, ledState);    // turn *on* led
+
+  analogWriteRange(255);
+  analogWriteFreq(200);
+
+  analogWrite(PWMPIN, 255); //inverted PWM, starting OFF 255
+
+  pinMode(BUTTON, INPUT_PULLUP);
+  strip.begin();
+
+  Serial.begin(115200);
+
+  // Connect to WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, pass);
+
+  int ledCount = 0;
+  while (WiFi.status() != WL_CONNECTED) {
+    ledCount = (ledCount + 1) % strip.numPixels();
+    if (ledCount == 0) {
+      strip.clear();
+      strip.show();
+    }
+    strip.setPixelColor(ledCount, strip.Color(255, 0, 0));
+    strip.show(); // Initialize all pixels to 'off'
+    delay(100);
+    Serial.print(".");
+  }
+  Serial.println("");
+
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  Serial.println("Starting UDP");
+  Udp.begin(inPort);
+  Serial.print("Local port: ");
+  Serial.println(Udp.localPort());
+
+
+  strip.clear();
+  strip.show(); // Initialize all pixels to 'off'
+
+
+}
+
+void loop() {
+  OSCMessage oscMessage;
+  int size = Udp.parsePacket();
+  if (size > 0) {
+    while (size--) {
+      oscMessage.fill(Udp.read());
+    }
+    if (oscMessage.fullMatch("/connected")) {
+      boolean response = oscMessage.getBoolean(0);
+      if (response) {
+        nState = WAIT;
+        digitalWrite(BUILTIN_LED, 0); //ON
+      }
+    } else if (oscMessage.fullMatch("/disconnect")) {
+      boolean response = oscMessage.getBoolean(0);
+      if (response) {
+        nState = DISCONNECT;
+        digitalWrite(BUILTIN_LED, 1); //OFF
+        strip.clear();
+        strip.show(); // Initialize all pixels to 'off'
+        analogWrite(PWMPIN, 255); //inverted PWM, starting OFF 255
+      }
+    } else if (oscMessage.fullMatch("/RGB")) { //
+
+      int pixelByte = 0;
+      //two 24 pixels ring, one will be 
+      //first 72 bytes 24 pixels
+      while (pixelByte < NUMPIXELS * 3) {
+        int r = oscMessage.getInt(pixelByte);
+        int g = oscMessage.getInt(pixelByte + 1);
+        int b = oscMessage.getInt(pixelByte + 2);
+        strip.setPixelColor(pixelByte / 3, strip.Color(r, g, b));
+        pixelByte += 3;
+      }
+      //next 3 bytes define the 24 next pixels color
+      int r = oscMessage.getInt(pixelByte);
+      int g = oscMessage.getInt(pixelByte + 1);
+      int b = oscMessage.getInt(pixelByte + 2);
+      
+      for(int i = 0; i < 24; i++){
+        strip.setPixelColor(24 + i, strip.Color(r, g, b));
+      }
+      strip.show(); // Initialize all pixels to 'off'
+      //last byte address number 75
+      int PWM = oscMessage.getInt(pixelByte + 3); //last BYTE is PWM
+      analogWrite(PWMPIN, 255 - PWM);
+    }
+
+  }
+
+  //sampling every 1s
+  if (millis() - lastMillis > 1000) {
+    lastMillis = millis();
+    switch (nState) {
+      case CONNECT: //send IP to connect and wait for income message
+
+        if (digitalRead(BUTTON) == 0) {
+          Serial.println("Trying to connect...");
+          sendMessage("/connect", WiFi.localIP().toString());
+          Serial.println("PRESSED");
+          uint16_t i, j;
+
+          for (j = 0; j < 256; j++) {
+            for (i = 0; i < strip.numPixels(); i++) {
+              strip.setPixelColor(i, Wheel((i + j) & 255));
+            }
+            strip.show();
+            delay(10);
+          }
+          strip.clear();
+          strip.show(); // Initialize all pixels to 'off'
+
+        }
+        break;
+      case WAIT:
+        Serial.println("Waiting...");
+        break;
+      case DISCONNECT:
+        Serial.println("Disconnecting...");
+        //sendMessage("/disconnect", WiFi.localIP().toString());
+        nState = CONNECT; //back to wait
+        break;
+    }
+  }
+}
+
+
+void sendMessage(String address, String data) {
+  OSCMessage msg(address.c_str());
+  msg.add(data.c_str());
+  Udp.beginPacket(outIp, outPort);
+  msg.send(Udp);
+  Udp.endPacket();
+  msg.empty();
+}
+
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if (WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if (WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
+
---
+++ Python-Server/artnet-server.py
@@ -0,0 +1,154 @@
+# Art-Net Server to OSC Message
+# The Kazimier Procession
+# Inspiration: https://github.com/bbx10/artnet-unicorn-hat
+# License: MIT
+
+import time
+import logging
+
+from twisted.internet.protocol import DatagramProtocol
+from twisted.internet import reactor
+
+from txosc import osc
+from txosc import dispatch
+from txosc import async
+
+
+FORMAT = '[%(levelname)s] (%(threadName)-10s) %(message)s'
+logging.basicConfig(level=logging.INFO, format=FORMAT)
+
+# Adjust the LED/ brightness as needed.
+NODES_NUM = 4 #number of nodesList
+NODES_ADDR = [72,1] #Number of addresses
+NODES_SIZE = sum(NODES_ADDR) #total address for the nodes and address
+NODES_MSG = ["RGB","PWM"]
+class ArtNet(DatagramProtocol):
+
+    def __init__(self, nodesServer):
+        self.nodesServer = nodesServer
+        self.artnetData =[0]* (NODES_NUM * sum(NODES_ADDR)) #allocate memory
+
+    def datagramReceived(self, data, (host, port)):
+        if ((len(data) > 18) and (data[0:8] == "Art-Net\x00")):
+            rawbytes = map(ord, data)
+            opcode = rawbytes[8] + (rawbytes[9] << 8)
+            protocolVersion = (rawbytes[10] << 8) + rawbytes[11]
+            if ((opcode == 0x5000) and (protocolVersion >= 14)):
+                sequence = rawbytes[12]
+                physical = rawbytes[13]
+                sub_net = (rawbytes[14] & 0xF0) >> 4
+                universe = rawbytes[14] & 0x0F
+                net = rawbytes[15]
+                length = (rawbytes[16] << 8) + rawbytes[17]
+                #print "seq %d phy %d sub_net %d uni %d net %d len %d" % (sequence, physical, sub_net, universe, net, length)
+    #                idx = 18
+                #print(rawbytes[18:length+18])
+                self.artnetData[universe*512:universe*512 + 512] = rawbytes[18:length+18] + (512-length)*[0] #chunks of 512
+                self.nodesServer.update(self.artnetData)
+                #print "\n\n"
+                #print self.artnetData
+
+class OSCNodesServer(object):
+    """
+    Example that sends and receives UDP OSC messages.
+    """
+    def __init__(self):
+        self.delay = 0.5
+        self.send_host = "127.0.0.1"
+        self.send_port = 8888
+        self.receive_port = 9999
+        self.nodesList = {}
+        self.rgbBytes = []
+        self.receiver = dispatch.Receiver()
+        self.sender = async.DatagramClientProtocol()
+        self._sender_port = reactor.listenUDP(0, self.sender)
+        self._server_port = reactor.listenUDP(self.receive_port, async.DatagramServerProtocol(self.receiver))
+
+        logging.info("Listening on osc.udp://localhost: {}".format(self.receive_port))
+
+        self.receiver.addCallback("/connect", self.connect)
+        self.receiver.addCallback("/ping", self.ping_handler)
+        self.receiver.addCallback("/pong", self.pong_handler)
+        self.receiver.fallback = self.fallback
+        reactor.callLater(0.1, self._start)
+
+    def update(self, rgbBytes):
+        self.rgbBytes = rgbBytes
+        for node, (ip, data) in enumerate(sorted(self.nodesList.iteritems(), key = lambda e: e[1])): #sort by the time of the conection
+            #node index
+            nodeChunck = self.rgbBytes[node*NODES_SIZE:node*NODES_SIZE + NODES_SIZE]
+            print(ip,nodeChunck)
+            port = data[0]
+            oscmsg = osc.Message("/RGB")
+            for b in nodeChunck:
+                oscmsg.add(b)
+            logging.info("Send disconnect {}".format(ip))
+            self.sender.send(oscmsg, (ip,port))
+
+    def _start(self):
+        """
+        Initiates the ping pong game.
+        """
+        self._send_ping()
+
+    def connect(self, message, address):
+        """
+        Method handler for /connect, new node
+        """
+        print("Got %s from %s" % (message, address))
+        ip = address[0]
+        port = address[1]
+        self.nodesList[ip] = [port,time.time(), ""]
+        self.sender.send(osc.Message("/connected"), (ip, port))
+
+    def ping_handler(self, message, address):
+        """
+        Method handler for /ping
+        """
+        print("Got %s from %s" % (message, address))
+        reactor.callLater(self.delay, self._send_pong)
+
+    def pong_handler(self, message, address):
+        """
+        Method handler for /pong
+        """
+        print("Got %s from %s" % (message, address))
+        reactor.callLater(self.delay, self._send_ping)
+
+    def _send_ping(self):
+        """
+        Sends /ping
+        """
+        self.sender.send(osc.Message("/ping"), (self.send_host, self.send_port))
+        print("Sent /ping")
+
+    def _send_pong(self):
+        """
+        Sends /pong
+        """
+        self.sender.send(osc.Message("/pong"), (self.send_host, self.send_port))
+        print("Sent /pong")
+
+    def fallback(self, message, address):
+        """
+        Fallback for everything else we get.
+        """
+        print("Lost ball %s from %s" % (message, address))
+
+    def shutdown(self):
+        #send disconnect command do nodes
+        for ip, data in self.nodesList.iteritems():
+            port = data[0]
+            logging.info("Send disconnect {}".format(ip))
+            self.sender.send(osc.Message("/disconnect", True), (ip,port))
+            time.sleep(0.5)
+
+
+if __name__ == "__main__":
+
+
+    oscServer = OSCNodesServer()
+    artNet =  ArtNet(oscServer)
+    reactor.listenUDP(6454, artNet)
+    reactor.addSystemEventTrigger('before', 'shutdown', oscServer.shutdown)
+    reactor.run()
